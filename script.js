4dd61a24a1bcdb7e658bc94cc6305dd857c006bc
Bercin <bercintatar@gmail.com>
2023-01-14 03:07:19 +0300

    add escape keydown function

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ for (let i = 0; i < showModalBtns.length; i++) {
 closeModalBtn.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
