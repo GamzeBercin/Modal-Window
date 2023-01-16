1cf3ee20b2acb909dfb3f0ea7819cca470429b07
Bercin <bercintatar@gmail.com>
2023-01-16 07:09:30 +0300

    add toggle method

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ const showModalBtns = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const openCloseModal = function () {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
 };
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
 for (let i = 0; i < showModalBtns.length; i++) {
-  showModalBtns[i].addEventListener('click', openModal);
+  showModalBtns[i].addEventListener('click', openCloseModal);
 }
 
-closeModalBtn.addEventListener('click', closeModal);
+closeModalBtn.addEventListener('click', openCloseModal);
 
-overlay.addEventListener('click', closeModal);
+overlay.addEventListener('click', openCloseModal);
 
 document.addEventListener('keydown', function (e) {
   console.log(e.key);
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
+    opencloseModal();
   }
 });
